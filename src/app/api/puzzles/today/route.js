2464c82667eb4fa82d57199a481ee744c7ba1784
treinfelder <treinfelder@gmail.com>
2026-09-15 02:46:38 -0400

    fix api import paths

--- src/app/api/puzzles/today/route.js
+++ src/app/api/puzzles/today/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import { getAllPuzzles } from "../../../lib/puzzles";
+import { getTodayPuzzle } from "../../../lib/puzzles";
+
 
 export function GET() {
   return NextResponse.json(getTodayPuzzle());
